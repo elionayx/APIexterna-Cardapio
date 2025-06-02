c4ad8ac51ae31237e552a3e6cd7c3c488b0e9e46
Eli <164237237+elionayx@users.noreply.github.com>
2025-06-02 20:51:29 -0300

    Update CardapioController.java

--- src/main/java/com/example/mockapi/cardapio_mock_api/controller/CardapioController.java
+++ src/main/java/com/example/mockapi/cardapio_mock_api/controller/CardapioController.java
@@ -1,4 +1,3 @@
-// src/main/java/com/example/mockapi/controller/CardapioController.java
 package com.example.mockapi.cardapio_mock_api.controller;
 
 import com.example.mockapi.cardapio_mock_api.model.ItemSimuladoDTO;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cardapio") // O prefixo base para os endpoints desta API
+@RequestMapping("/cardapio") 
 public class CardapioController {
     private final CardapioService cardapioService;
 
